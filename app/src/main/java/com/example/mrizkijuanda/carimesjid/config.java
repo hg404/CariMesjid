e592c35f120b18ddb3e841af37d8a5a04c044671
Muhammad Rizki Juanda <muhammadrizkijuanda@gmail.com>
2016-06-11 10:39:37 +0700

    Update ......,,

--- app/src/main/java/com/example/mrizkijuanda/carimesjid/config.java
+++ app/src/main/java/com/example/mrizkijuanda/carimesjid/config.java
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 package com.example.mrizkijuanda.carimesjid;
 
 \
@@ -28,3 +29,35 @@ public class config {
     public static final String EMP_ID = "emp_id";
 
 }
+=======
+package com.example.mrizkijuanda.carimesjid;
+
+
+public class config {
+    public static final String URL_GET_ALL_BANDAACEH="http://android.jones.id/getALLBandaAceh.php";
+    public static final String URL_GET_BANDAACEH="http://android.jones.id/getBandaAceh.php?id=";
+
+
+    public static final String URL_GET_ALL_ACEHBESAR="http://android.jones.id/getALLAcehBesar.php";
+    public static final String URL_GET_ACEHBESAR="http://android.jones.id/getAcehBesar.php?id=";
+
+
+    public static final String URL_GET_ALL_PIDIE="http://android.jones.id/getALLPidie.php";
+    public static final String URL_GET_PIDIE="http://android.jones.id/getPidie.php?id=";
+
+
+    public static final String URL_GET_ALL_ACEHJAYA="http://android.jones.id/getALLAcehJaya.php";
+    public static final String URL_GET_ACEHJAYA="http://android.jones.id/getAcehJaya.php?id=";
+
+    //JSON Tags....
+    public static final String TAG_JSON_ARRAY="result";
+    public static final String TAG_ID = "id";
+    public static final String TAG_NAMA = "nama";
+
+    public static final String TAG_LOKASILAT = "langtitude";
+    public static final String TAG_LOKASILONG = "longtitude";
+
+    public static final String EMP_ID = "emp_id";
+
+}
+>>>>>>> Stashed changes
